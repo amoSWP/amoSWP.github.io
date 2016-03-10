800021ca316fef7e3b827a1abb56ac239602a389
Victor Lauritz Jacob <victorjacob@llanberis.imp.fu-berlin.de>
2016-03-10 14:29:11 +0100

    Bewegung hoch und runter hinzugefügt

--- core/src/com/dive/game/DiveGame.java
+++ core/src/com/dive/game/DiveGame.java
@@ -2,26 +2,70 @@ package com.dive.game;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
+import com.badlogic.gdx.graphics.Color;
 import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class DiveGame extends ApplicationAdapter {
-	SpriteBatch batch;
-	Texture img;
+	private SpriteBatch batch;
+	private BitmapFont font;
+	private Texture background;
+	private Texture divertex;
+	private Sprite diversprite;
 	
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-		img = new Texture("badlogic.jpg");
-	}
+		font = new BitmapFont();
+		font.setColor(Color.BLACK);
+		// Background
+		background = new Texture("Background.jpg");
+		// Moveable Objects
+		divertex = new Texture(Gdx.files.internal("jet.png"));
+		diversprite = new Sprite(divertex);
+	}	
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
-		batch.begin();
-		batch.draw(img, 0, 0);
+		// Draw Background
+		batch.draw(background, 0, 0);
+		// Movement of Jet
+		// Movement Up
+        if(Gdx.input.isKeyPressed(Input.Keys.UP)){
+        	// If Control is pressed move 1 pixel else 10
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                diversprite.translateY(1f);
+            else
+                diversprite.translateY(10.0f);
+        }
+        // Movement Down
+        if(Gdx.input.isKeyPressed(Input.Keys.DOWN)){
+        	// If Control is pressed 1 pixel else 10
+            if(Gdx.input.isKeyPressed(Input.Keys.CONTROL_LEFT))
+                diversprite.translateY(-1f);
+            else
+                diversprite.translateY(-10.0f);
+        }
+		batch.begin();		
+		diversprite.draw(batch);
 		batch.end();
+	}
+	
+	@Override
+	public void resize(int width, int height){	
+	}
+	
+	@Override
+	public void pause(){
+	}
+	
+	@Override
+	public void resume(){	
 	}
 }
