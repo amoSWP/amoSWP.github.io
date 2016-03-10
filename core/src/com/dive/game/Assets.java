a1163013a8fff57233f6e051d7ff2210a6fc2a92
tonnrueter <rueter@physik.fu-berlin.de>
2016-03-10 16:39:12 +0100

    Assets is a singleton class to turn images into textures (and later sounds and so on)

---
+++ core/src/com/dive/game/Assets.java
@@ -0,0 +1,59 @@
+package com.dive.game;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.audio.Sound;
+
+// From https://github.com/libgdx/libgdx-demo-superjumper/blob/master/core/src/com/badlogicgames/superjumper/Assets.java
+// Extended by Singleton-mechanics
+
+public class Assets {
+	
+	// Assets is Singleton
+	
+	private static Assets instance = null;
+	
+	/*
+	 * 	TEXTUREN
+	 */
+	public Texture backgroundFront;
+	public Texture backgroundMiddle;
+	public Texture backgroundRear;
+	//public TextureRegion backgroundRegion;
+	
+	/*
+	 * 	SOUNDS
+	 */
+	//public Sound makeSomeNoise;
+	
+	protected Assets() {
+		// Empty
+	}
+	
+	public static Assets getInstance() {
+		if (instance == null) {
+			instance = new Assets();
+			instance.load();
+		}
+		return instance;
+	}
+
+	private static Texture loadTexture (String location) {
+		return new Texture(Gdx.files.internal(location));
+		//return new Texture(location);
+	}
+
+	private void load () {
+		
+		System.out.println("Loading Assets..");
+		
+		backgroundFront  = loadTexture("background/BottomBackground.png");
+		backgroundMiddle = loadTexture("background/MainBackground.jpg");
+		backgroundRear   = loadTexture("background/TopBackground 2.png");
+		
+	}
+
+	//public static void playSound (Sound sound) {
+	//	if (Settings.soundEnabled) sound.play(1);
+	//}
+}
