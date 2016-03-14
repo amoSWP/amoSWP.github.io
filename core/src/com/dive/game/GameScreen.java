cde0ece9c9b7ba471d7262bb22c4df18f662f881
Aiko Pipo <aiko@corris.imp.fu-berlin.de>
2016-03-14 16:43:02 +0100

    Basis Klassen

---
+++ core/src/com/dive/game/GameScreen.java
@@ -0,0 +1,14 @@
+package com.dive.game;
+
+public class GameScreen {
+
+	public int width, height, x, y;	//Darstellungsbereich angeben
+	
+	public GameScreen(int width, int height, int x, int y){
+		this.width = width;
+		this.height = height;
+		this.x = x;
+		this.y = y;
+	}
+	
+}
