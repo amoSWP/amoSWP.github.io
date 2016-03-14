abeb165fca5c9344a764d274ed896e3d7771defd
Lisa Gaedke-Merzhaeuser <lisagaedke@tywyn.imp.fu-berlin.de>
2016-03-14 15:45:00 +0100

    neue ObjektGenerator Klasse geschrieben, Divegame, Ente, Assets geändert

---
+++ core/src/com/dive/game/ObjectGenerator.java
@@ -0,0 +1,68 @@
+package com.dive.game;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.badlogic.gdx.Gdx;
+
+public class ObjectGenerator {
+	Ente[] listEnten;
+	float countDown;
+	float maxCountDown;
+	int pointer;
+
+	int maxNoEnte;
+	int min = 70;
+	int max = 390;
+
+	// constructor: kreiere Liste mit Enten
+	public ObjectGenerator(int maxNoEnte) {
+		pointer = 0;
+		this.maxNoEnte = maxNoEnte;
+		countDown = maxCountDown = 1;
+		listEnten = new Ente[maxNoEnte];
+		for (int i = 0; i < maxNoEnte; i++) {
+			listEnten[i] = new Ente(Gdx.graphics.getWidth(), randomInteger(min,
+					max), 100, 100, -3.0f, Assets.getInstance().ente);
+		}
+	}
+
+	// gehe Liste der Enten durch und erstelle neue Liste von Enten welche genau
+	// so gezeichnet werden soll
+	public void nextObject(ArrayList<Ente> list, float deltaTime) {
+		countDown -= deltaTime;
+		
+		// überprüft ob Zeit abgelaufen und Objekt nicht aktiv, schreibt in
+		// Liste um dann gezeichnet zu werden
+		if (countDown < 0 && !listEnten[pointer].active) {
+			list.add(listEnten[pointer]);
+
+			listEnten[pointer].active = true;
+			pointer = (pointer + 1) % maxNoEnte;
+			countDown = maxCountDown + randomInteger(0, 2);
+		}
+
+		// wenn Objekt Bildschirmrand erreicht wird es aus Liste gestrichen, auf
+		// Ausgangsposition gesetzt und Status auf nicht aktiv, steht nun wieder
+		// zur Verfügung
+		for (int i = 0; i < maxNoEnte; i++) {
+			Ente e = listEnten[i];
+			if (e.active && (e.sprite.getX() < -e.sprite.getWidth())) {
+
+				e.active = false;
+				list.remove(e);
+				e.sprite.setX(Gdx.graphics.getWidth());
+				e.sprite.setY(randomInteger(min, max));
+			}
+		}
+	}
+
+	//Zufällige Erzeugung von integer Werten zwischen min max
+	public int randomInteger(int min, int max) {
+		Random rand = new Random();
+		int randomNum = rand.nextInt((max - min) + 1) + min;
+		return randomNum;
+	}
+
+}
